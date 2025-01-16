814135562d93d0591ba48e3a393cd052bb00fb88
Nikita Akilov <26031301+n5a5@users.noreply.github.com>
2025-01-16 19:57:40 +0100

    dbeaver/dbeaver-infra#193 create dbeaver dev environment with 1 command

--- runGenerator.sh
+++ runGenerator.sh
@@ -13,13 +13,11 @@ if [ -z "$WORKING_DIR" ]; then
   echo "No folder containing rcp_gen specified"
   exit 1
 fi
-cd aggregate
-mvn install -q
-cd ..
-# Run the Maven commands with the specified options
-mvn -f "pom.xml" \
+
+maven="../dbeaver-common/root/mvnw -T 1C"
+$maven install -q -f "aggregate"
+$maven -f "pom.xml" \
     package \
-    -T 1C \
     -q \
     exec:java \
     -Dexec.args="-eclipse.version \${eclipse-version} -config $WORKING_DIR/osgi-app.properties -projectsFolder $WORKING_DIR/../ -eclipse $WORKING_DIR/../dbeaver-workspace/dependencies -output $WORKING_DIR/../dbeaver-workspace/products/"
