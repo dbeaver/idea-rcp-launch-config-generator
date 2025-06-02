45dc6bfb94c1432c1b5dcb672cd2d4d5b85dc7b3
IvanGorshechnikov <ivan.gorshechnikov1@gmail.com>
2025-06-02 17:32:07 +0200

    dbeaver/pro#5774 Add mvn args to sh script to fix generating on java 24

--- runGenerator.sh
+++ runGenerator.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+mvn_args="-Djdk.xml.maxGeneralEntitySizeLimit=2097152 -Djdk.xml.totalEntitySizeLimit=2097152"
 target_repo="$1"
 if [ -z "$target_repo" ]; then
   echo "target repository is not specified"
@@ -12,14 +13,15 @@ script_dir="$(realpath "$(dirname "$0")")"
 repositories_root_dir="$(realpath "$script_dir/..")"
 
 echo "Compiling workspace generator dependencies..."
+"$repositories_root_dir/dbeaver-common/mvnw" --version
 
 "$repositories_root_dir/dbeaver-common/mvnw" install \
-    -T1C \
+    -T1C $mvn_args \
     -q \
     -f "$script_dir/aggregate"
 
 "$repositories_root_dir/dbeaver-common/mvnw" package \
-    -T1C \
+    -T1C $mvn_args \
     -q \
     -f "$script_dir/pom.xml" \
     exec:java \
