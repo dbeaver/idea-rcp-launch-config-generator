558c41b6958ce39e14e5c82cc9033a40dc1d5b0f
serge-rider <serge@dbeaver.com>
2025-06-09 19:36:55 +0200

    dbeaver/pro#5936 Code style

--- src/main/java/org/jkiss/tools/rcplaunchconfig/EntryPoint.java
+++ src/main/java/org/jkiss/tools/rcplaunchconfig/EntryPoint.java
@@ -39,7 +39,6 @@ import org.jkiss.utils.CommonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -49,11 +48,12 @@ import java.util.Map;
 import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javax.xml.stream.XMLStreamException;
 
 public class EntryPoint {
     private static final Logger log = LoggerFactory.getLogger(EntryPoint.class);
 
-    public static void main(String[] args) throws IOException, XMLStreamException, RepositoryInitialisationError {
+    public static void main(String[] args) throws RepositoryInitialisationError {
         try {
             launchGenerate(args);
         } catch (Exception exception) {
@@ -61,7 +61,7 @@ public class EntryPoint {
         }
     }
 
-    private static void launchGenerate(String[] args) throws IOException, RepositoryInitialisationError, XMLStreamException {
+    private static void launchGenerate(String[] args) throws IOException, RepositoryInitialisationError {
         var params = new Params();
         log.info("Process started with the following arguments: " + Arrays.toString(args));
         params.init(args);
@@ -72,76 +72,70 @@ public class EntryPoint {
         } else {
             logger.setLevel(Level.INFO);
         }
-        ForkJoinPool forkJoinPool;
-        if (params.singleCoreMode) {
-            forkJoinPool = new ForkJoinPool(1);
-        } else {
-            forkJoinPool = ForkJoinPool.commonPool();
-        }
-        log.info("Dependency folder location: " + params.eclipsePath);
+        try (ForkJoinPool forkJoinPool = createForkJoinPool(params)) {
+            log.info("Dependency folder location: " + params.eclipsePath);
 
-        var settings = ConfigFileManager.readSettingsFile(params.configFilePath);
+            var settings = ConfigFileManager.readSettingsFile(params.configFilePath);
 
-        var pathsManager = PathsManager.INSTANCE;
-        pathsManager.init(settings, params.projectsFolderPath, params.eclipsePath);
-        P2RepositoryManager p2RepositoryManager = P2RepositoryManager.INSTANCE;
-        p2RepositoryManager.init(settings, params.eclipseVersion, null);
-        if (log.isDebugEnabled()) {
-            var featuresPaths = pathsManager.getFeaturesLocations().stream()
-                .map(it -> it.toAbsolutePath().toString())
-                .collect(Collectors.joining("\n  "));
-            var bundlesPaths = pathsManager.getBundlesLocations().stream()
-                .map(it -> it.toAbsolutePath().toString())
-                .collect(Collectors.joining("\n  "));
-            var productPaths = pathsManager.getProductsPathsAndWorkDirs().entrySet().stream()
-                .map(it -> it.getKey().toAbsolutePath().toString())
-                .collect(Collectors.joining("\n  "));
-            log.debug("Search dependencies for '{}' in Eclipse folder '{}' and projects:\n  {}\n  {}",
-                productPaths,
-                pathsManager.getEclipsePluginsPath(),
-                featuresPaths,
-                bundlesPaths
-            );
-        }
-        List<ResultInfo> executionResults = forkJoinPool.submit(() -> pathsManager.getProductsPathsAndWorkDirs().entrySet().parallelStream().map((productPath) -> {
-            log.info("Starting generation for {}", productPath);
-            log.debug("Thread name {} used for {}", Thread.currentThread().getName(), productPath);
-            try {
-                Result result = new Result();
-                result.setWorkDir(productPath.getValue());
-                result.setProductPath(productPath.getKey());
-                DependencyGraph dependencyGraph = getGraphImplementation(generateDependencyTree);
-                result.setProductGraph(dependencyGraph);
-                FeatureResolver.addNewFeatureProject(result.getProductPath());
-                dependencyGraph.traverseIntoNode(result.getProductPath().toString());
-                DependencyGraph.DependencyNode root = dependencyGraph.getCurrentNode();
-                XmlReader.INSTANCE.parseXmlFile(result, productPath.getKey().toFile(), dependencyGraph);
-                dependencyGraph.setCurrentNode(root);
-                new DynamicImportsResolver(IMLConfigurationProducer.INSTANCE)
-                    .start(result, p2RepositoryManager.getLookupCache(), dependencyGraph);
-                dependencyGraph.setCurrentNode(root);
-                var resultPath = params.resultFilesPath;
-                resultPath = resultPath.resolve(productPath.getKey().getFileName());
-                try {
-                    Files.createDirectories(resultPath.getParent());
-                } catch (Throwable throwable) {
-                    log.debug("Error creating target parent directories for {}", resultPath);
-                }
-                try {
-                    FileUtils.removeAllFromDir(resultPath);
-                } catch (Throwable e) {
-                    log.debug("Error deleting target folder for {}", resultPath, e);
-                }
-                log.info("Product generation for {} completed", result.getProductId());
-                log.debug("Thread {} finished execution", Thread.currentThread().getName());
-                dependencyGraph.printDependencyTree(root);
-                return new ResultInfo(productPath, result, resultPath);
-            } catch (XMLStreamException | IOException e) {
-                throw new RuntimeException(e);
+            var pathsManager = PathsManager.INSTANCE;
+            pathsManager.init(settings, params.projectsFolderPath, params.eclipsePath);
+            P2RepositoryManager p2RepositoryManager = P2RepositoryManager.INSTANCE;
+            p2RepositoryManager.init(settings, params.eclipseVersion, null);
+            if (log.isDebugEnabled()) {
+                var featuresPaths = pathsManager.getFeaturesLocations().stream()
+                    .map(it -> it.toAbsolutePath().toString())
+                    .collect(Collectors.joining("\n  "));
+                var bundlesPaths = pathsManager.getBundlesLocations().stream()
+                    .map(it -> it.toAbsolutePath().toString())
+                    .collect(Collectors.joining("\n  "));
+                var productPaths = pathsManager.getProductsPathsAndWorkDirs().keySet().stream()
+                    .map(s -> s.toAbsolutePath().toString())
+                    .collect(Collectors.joining("\n  "));
+                log.debug("Search dependencies for '{}' in Eclipse folder '{}' and projects:\n  {}\n  {}",
+                    productPaths,
+                    pathsManager.getEclipsePluginsPath(),
+                    featuresPaths,
+                    bundlesPaths
+                );
             }
-        }).collect(Collectors.toList())).join();
-        forkJoinPool.submit(() -> {
-            executionResults.parallelStream().forEach((executionResult) -> {
+            List<ResultInfo> executionResults = forkJoinPool.submit(() -> pathsManager.getProductsPathsAndWorkDirs().entrySet().parallelStream().map((productPath) -> {
+                log.info("Starting generation for {}", productPath);
+                log.debug("Thread name {} used for {}", Thread.currentThread().getName(), productPath);
+                try {
+                    Result result = new Result();
+                    result.setWorkDir(productPath.getValue());
+                    result.setProductPath(productPath.getKey());
+                    DependencyGraph dependencyGraph = getGraphImplementation(generateDependencyTree);
+                    result.setProductGraph(dependencyGraph);
+                    FeatureResolver.addNewFeatureProject(result.getProductPath());
+                    dependencyGraph.traverseIntoNode(result.getProductPath().toString());
+                    DependencyGraph.DependencyNode root = dependencyGraph.getCurrentNode();
+                    XmlReader.INSTANCE.parseXmlFile(result, productPath.getKey().toFile(), dependencyGraph);
+                    dependencyGraph.setCurrentNode(root);
+                    new DynamicImportsResolver(IMLConfigurationProducer.INSTANCE)
+                        .start(result, p2RepositoryManager.getLookupCache(), dependencyGraph);
+                    dependencyGraph.setCurrentNode(root);
+                    var resultPath = params.resultFilesPath;
+                    resultPath = resultPath.resolve(productPath.getKey().getFileName());
+                    try {
+                        Files.createDirectories(resultPath.getParent());
+                    } catch (Throwable throwable) {
+                        log.debug("Error creating target parent directories for {}", resultPath);
+                    }
+                    try {
+                        FileUtils.removeAllFromDir(resultPath);
+                    } catch (Throwable e) {
+                        log.debug("Error deleting target folder for {}", resultPath, e);
+                    }
+                    log.info("Product generation for {} completed", result.getProductId());
+                    log.debug("Thread {} finished execution", Thread.currentThread().getName());
+                    dependencyGraph.printDependencyTree(root);
+                    return new ResultInfo(productPath, result, resultPath);
+                } catch (XMLStreamException | IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).collect(Collectors.toList())).join();
+            forkJoinPool.submit(() -> executionResults.parallelStream().forEach((executionResult) -> {
                 try {
                     Path resultPath = executionResult.resultPath();
                     Result result = executionResult.result;
@@ -177,49 +171,57 @@ public class EntryPoint {
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-            });
-        }).join();
-        log.info("Product generation completed for all products!");
-        forkJoinPool.shutdown();
-        List<Path> additionalLibraries = PathsManager.INSTANCE.getAdditionalLibraries();
-        log.info("Appending additional libraries...");
-        if (additionalLibraries != null) {
-            for (Path additionalLibrary : additionalLibraries) {
-                FileUtils.copyFolder(additionalLibrary, PathsManager.INSTANCE.getEclipsePath(), false);
-            }
-        }
-        log.info("Resolving additional repositories...");
-        if (!CommonUtils.isEmpty(pathsManager.getAdditionalRepositoriesPaths())) {
-            Result result = new Result();
-            result.setProductPath(Path.of("/"));
-            FeatureResolver.addNewFeatureProject(result.getProductPath());
-            DependencyGraph dependencyGraph = getGraphImplementation(generateDependencyTree);
-            dependencyGraph.traverseIntoNode("additional");
-            DependencyGraph.DependencyNode root = dependencyGraph.getCurrentNode();
-            for (Path additionalRepositoriesPath : pathsManager.getAdditionalRepositoriesPaths()) {
-                try (Stream<Path> stream = Files.walk(additionalRepositoriesPath)) {
-                    List<Path> categoryXMLS = stream.filter(Files::isRegularFile)
-                        .filter(path -> path.getFileName().toString().equals("category.xml"))
-                        .toList();
-                    for (Path categoryXML : categoryXMLS) {
-                        log.debug("Generating config for " + categoryXML);
-                        CategoryXMLFileParser.parseCategoryXML(result, categoryXML, dependencyGraph);
-                    }
-                } catch (IOException e) {
-                    log.error("Error reading the repository " + e);
+            })).join();
+
+            log.info("Product generation completed for all products!");
+            List<Path> additionalLibraries = PathsManager.INSTANCE.getAdditionalLibraries();
+            log.info("Appending additional libraries...");
+            if (additionalLibraries != null) {
+                for (Path additionalLibrary : additionalLibraries) {
+                    FileUtils.copyFolder(additionalLibrary, PathsManager.INSTANCE.getEclipsePath(), false);
                 }
             }
-            dependencyGraph.printDependencyTree(root);
-            log.debug(result.getBundlesByNames().size() + " additional bundles to resolve found");
+            log.info("Resolving additional repositories...");
+            if (!CommonUtils.isEmpty(pathsManager.getAdditionalRepositoriesPaths())) {
+                Result result = new Result();
+                result.setProductPath(Path.of("/"));
+                FeatureResolver.addNewFeatureProject(result.getProductPath());
+                DependencyGraph dependencyGraph = getGraphImplementation(generateDependencyTree);
+                dependencyGraph.traverseIntoNode("additional");
+                DependencyGraph.DependencyNode root = dependencyGraph.getCurrentNode();
+                for (Path additionalRepositoriesPath : pathsManager.getAdditionalRepositoriesPaths()) {
+                    try (Stream<Path> stream = Files.walk(additionalRepositoriesPath)) {
+                        List<Path> categoryXMLS = stream.filter(Files::isRegularFile)
+                            .filter(path -> path.getFileName().toString().equals("category.xml"))
+                            .toList();
+                        for (Path categoryXML : categoryXMLS) {
+                            log.debug("Generating config for " + categoryXML);
+                            CategoryXMLFileParser.parseCategoryXML(result, categoryXML, dependencyGraph);
+                        }
+                    } catch (IOException e) {
+                        log.error("Error reading the repository " + e);
+                    }
+                }
+                dependencyGraph.printDependencyTree(root);
+                log.debug(result.getBundlesByNames().size() + " additional bundles to resolve found");
 
-            IMLConfigurationProducer.INSTANCE.generateIMLFiles(result, null);
+                IMLConfigurationProducer.INSTANCE.generateIMLFiles(result, null);
+            }
+            log.info("Producing final IML configuration...");
+            IMLConfigurationProducer.INSTANCE.generateImplConfiguration();
+            if (params.updateWorkspace) {
+                IMLConfigurationProducer.INSTANCE.patchWorkspaceParameters();
+            }
+            log.info("Execution completed!");
         }
-        log.info("Producing final IML configuration...");
-        IMLConfigurationProducer.INSTANCE.generateImplConfiguration();
-        if (params.updateWorkspace) {
-            IMLConfigurationProducer.INSTANCE.patchWorkspaceParameters();
+    }
+
+    private static ForkJoinPool createForkJoinPool(Params params) {
+        if (params.singleCoreMode) {
+            return new ForkJoinPool(1);
+        } else {
+            return ForkJoinPool.commonPool();
         }
-        log.info("Execution completed!");
     }
 
     @NotNull
