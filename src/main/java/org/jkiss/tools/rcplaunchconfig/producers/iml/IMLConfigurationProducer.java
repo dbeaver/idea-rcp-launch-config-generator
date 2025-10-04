487384b7ea1c57579126714d4b8599d0db408dac
serge-rider <serge@dbeaver.com>
2025-10-04 20:32:42 +0200

    Pass launcher path to debug configs

--- src/main/java/org/jkiss/tools/rcplaunchconfig/producers/iml/IMLConfigurationProducer.java
+++ src/main/java/org/jkiss/tools/rcplaunchconfig/producers/iml/IMLConfigurationProducer.java
@@ -243,7 +243,7 @@ public class IMLConfigurationProducer implements IImportListener {
         config.append(getFormattedRelativePath(productPath, false, true, true)).append("&quot; ");
         config.append("-dev &quot;");
         config.append(getFormattedRelativePath(productPath, false, true, true)).append("/dev.properties").append("&quot; ");
-        config.append("-nl en -consoleLog -showsplash ");
+        config.append("-nl en -consoleLog -showsplash -launcher ").append(System.getenv("JAVA_HOME")).append("/bin/java\n");
         if (result.getArguments().getProgramARGS() != null) {
             for (String programARG : result.getArguments().getProgramARGS()) {
                 config.append(programARG).append(" ");
