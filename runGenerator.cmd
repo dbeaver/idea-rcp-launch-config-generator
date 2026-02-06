53ac97017ac61803c0ba8328f0c7a038aab040e1
ShadelessFox <yanuari.channel@gmail.com>
2026-02-06 17:27:51 +0100

    dbeaver/pro#7071 Update generator to clone and build checkstyle check

--- runGenerator.cmd
+++ runGenerator.cmd
@@ -24,10 +24,17 @@ if "%WORKING_DIR%"=="" (
     goto :end
 )
 
-echo Build generator
+echo Build workspace generator
 call %GENERATOR_DIR%..\dbeaver-common\mvnw.cmd clean install %MAVEN_ARGS% -q -f "%GENERATOR_DIR%aggregate"
 
-echo Run generator
+call %GENERATOR_DIR%..\dbeaver-common\mvnw.cmd package %MAVEN_ARGS% -q -f "%GENERATOR_DIR%aggregate"
+
+"$repositories_root_dir/dbeaver-common/mvnw" package \
+    $mvn_args \
+    -q \
+    -f "%WORKING_DIR%..\dbeaver-workspace\checkstyle-nullability-annotations\pom.xml"
+
+echo Run workspace generator
 :: Run the Maven commands with the specified options
 call %GENERATOR_DIR%..\dbeaver-common\mvnw.cmd -f "%GENERATOR_DIR%pom.xml" package %MAVEN_ARGS% -q exec:java -Dexec.args="-eclipse.version ${eclipse-version} -updateWorkspace -config %WORKING_DIR%osgi-app.properties -projectsFolder %WORKING_DIR%..\ -eclipse %WORKING_DIR%..\dbeaver-workspace\dependencies -output %WORKING_DIR%..\dbeaver-workspace\products"
 
